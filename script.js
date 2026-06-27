2a4ae51c858960cc6b6038ca42e08e67cb617ee0
yusufahmshe-tech <yusufahmshe@gmail.com>
2026-06-28 01:32:14 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+async function translateText() {
+    const text = document.getElementById("inputText").value.trim();
+    const lang = document.getElementById("lang").value;
+    const [from, to] = lang.split("|");
+
+    const output = document.getElementById("output");
+
+    if (!text) {
+        output.innerText = "اكتب نص الأول";
+        return;
+    }
+
+    output.innerText = "جاري الترجمة...";
+
+    try {
+        const url = `https://api.mymemory.translated.net/get?q=${encodeURIComponent(text)}&langpair=${from}|${to}`;
+
+        const res = await fetch(url);
+        const data = await res.json();
+
+        if (data.responseData && data.responseData.translatedText) {
+            output.innerText = data.responseData.translatedText;
+        } else {
+            output.innerText = "مفيش نتيجة ترجمة";
+        }
+
+    } catch (error) {
+        console.log(error);
+        output.innerText = "حدث خطأ في الاتصال بالترجمة";
+    }
+}
